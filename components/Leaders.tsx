66c2d5b49e4ca7fb4bd6ee7ce8ffc3bface9c2c5
raysanchezcanto-max <raysanchez.canto@gmail.com>
2026-09-10 15:58:11 -0600

    Add Ramon Rios local photo fallback
    Add specific player photo handling for playerId 540204 in LMP league.

--- components/Leaders.tsx
+++ components/Leaders.tsx
@@ -5,9 +5,13 @@ function playerPhoto(
   league: LeagueCode,
   playerId: number
 ) {
- if (league === "LMP") {
-  return `https://images.cloudgfx.com/player/mugshot/${playerId}?app=lamp&h=350&w=250`;
-}
+  if (league === "LMP") {
+    if (playerId === 540204) {
+      return "/players/lmp/ramon-rios.jpg";
+    }
+
+    return `https://images.cloudgfx.com/player/mugshot/${playerId}?app=lamp&h=350&w=250`;
+  }
 
   return `https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_180,q_auto:best/v1/people/${playerId}/headshot/silo/current.png`;
 }
