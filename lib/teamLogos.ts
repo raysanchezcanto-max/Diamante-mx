2ff17516115a254fcbcdc8efefc0a484b09cdbd7
raysanchezcanto-max <raysanchez.canto@gmail.com>
2026-09-10 17:22:40 -0600

    Add LMB team logo catalog

--- lib/teamLogos.ts
+++ lib/teamLogos.ts
@@ -31,6 +31,78 @@ const LMP_LOGOS: Record<string, string> = {
     "/logos/lmp/mayos-navojoa.png",
   "venados de mazatlan":
     "/logos/lmp/venados-mazatlan.png",
+  const LMB_LOGOS: Record<string, string> = {
+  "toros de tijuana":
+    "/logos/lmb/toros-tijuana.png",
+
+  "caliente de durango":
+    "/logos/lmb/caliente-durango.png",
+
+  "sultanes de monterrey":
+    "/logos/lmb/sultanes-monterrey.png",
+
+  "charros de jalisco":
+    "/logos/lmb/charros-jalisco.png",
+
+  "acereros del norte":
+    "/logos/lmb/acereros-monclova.png",
+
+  "acereros de monclova":
+    "/logos/lmb/acereros-monclova.png",
+
+  "algodoneros union laguna":
+    "/logos/lmb/algodoneros-union-laguna.png",
+
+  "algodoneros de union laguna":
+    "/logos/lmb/algodoneros-union-laguna.png",
+
+  "rieleros de aguascalientes":
+    "/logos/lmb/rieleros-aguascalientes.png",
+
+  "saraperos de saltillo":
+    "/logos/lmb/saraperos-saltillo.png",
+
+  "tecos de los dos laredos":
+    "/logos/lmb/tecos-dos-laredos.png",
+
+  "dorados de chihuahua":
+    "/logos/lmb/dorados-chihuahua.png",
+
+  "diablos rojos del mexico":
+    "/logos/lmb/diablos-rojos-mexico.png",
+
+  "diablos rojos del méxico":
+    "/logos/lmb/diablos-rojos-mexico.png",
+
+  "olmecas de tabasco":
+    "/logos/lmb/olmecas-tabasco.png",
+
+  "piratas de campeche":
+    "/logos/lmb/piratas-campeche.png",
+
+  "pericos de puebla":
+    "/logos/lmb/pericos-puebla.png",
+
+  "bravos de leon":
+    "/logos/lmb/bravos-leon.png",
+
+  "guerreros de oaxaca":
+    "/logos/lmb/guerreros-oaxaca.png",
+
+  "el aguila de veracruz":
+    "/logos/lmb/aguila-veracruz.png",
+
+  "aguila de veracruz":
+    "/logos/lmb/aguila-veracruz.png",
+
+  "tigres de quintana roo":
+    "/logos/lmb/tigres-quintana-roo.png",
+
+  "conspiradores de queretaro":
+    "/logos/lmb/conspiradores-queretaro.png",
+
+  "leones de yucatan":
+    "/logos/lmb/leones-yucatan.png",
 };
 
 function normalizeTeamName(name: string) {
