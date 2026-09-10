24782d3a57180b2901b19b530994361c9394dedf
raysanchezcanto-max <raysanchez.canto@gmail.com>
2026-09-10 16:36:07 -0600

    Remove unnecessary LMB game type filter
    Removed unnecessary query parameters from the schedule fetch URL.

--- lib/lmb.ts
+++ lib/lmb.ts
@@ -64,7 +64,7 @@ export async function getLmbGames():
     const date = mexicoDate();
 
     const data = await lmbFetch(
-  `/schedule?sportId=${LMB_SPORT_ID}&leagueId=${LMB_LEAGUE_ID}&date=${date}&season=${seasonYear()}&gameTypes=R,F,D,L,W&hydrate=linescore`
+  `/schedule?sportId=${LMB_SPORT_ID}&leagueId=${LMB_LEAGUE_ID}&date=${date}&hydrate=linescore`
 );
     const games =
       data?.dates?.flatMap(
